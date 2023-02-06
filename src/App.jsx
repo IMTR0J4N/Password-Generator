cae67c60b9905aad7faa1106f42c253b1fc02252
jokay03J-V2 <jmagro@edenschool.fr>
2023-02-06 16:26:16 +0100

    update Home page
    *add Button & add style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import "./App.css";
+import Box from "./components/Box";
+import Button from "./components/Button";
+import { generatePassword } from "./utils/generatePassword";
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Box className="h--screen w--screen bg--black--light--1 d--flex justify--center App">
+      <Box className={"w--4-12 h--7-12 bg--black--light--2 p--2"}>
+        <Button style={{ textTransform: "uppercase", fontFamily: "Poppins, sans-serif" }} onClick={() => console.log(generatePassword(10, { lowercase: true, uppercase: true, number: true, symbols: true }))}>generate →</Button>
+      </Box>
+    </Box>
   )
 }
 
