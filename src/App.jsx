a1f4b6c9274477d38c0156201b7711556f4e24bb
Ilyansm <cjeanpierre@edenschool.fr>
2023-02-07 16:41:20 +0100

    not finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import CopyButton from "./components/CopyButton"
+
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <CopyButton onClick={() => { navigator.clipboard.writeText(this.state.textToCopy) }}></CopyButton>
     </>
   )
 }
---
+++ src/components/CopyButton.jsx
@@ -0,0 +1,7 @@
+export default function CopyButton() {
+    return (
+        <img src="icon-copy.svg"></img>
+    )
+
+
+}
